d3c4f553fb3977aed1a7b1623e20b9ef26b5e838
unknown <Desofiyamariya@gmail.com>
2021-12-10 11:33:18 +0200

    initial commit

---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,22 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../5.2.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			int a = 1;
+			int x = 0;
+			int n = 1;
+			double c = A(x, a, n);
+			Assert::AreEqual(c, 0.);
+		}
+	};
+}
